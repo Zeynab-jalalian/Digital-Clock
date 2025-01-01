ae9bdb38479df9c10ce47bca36891fab048b0b74
zeynab jalalian <zjalalianweb@gmail.com>
2025-01-01 12:09:24 +0330

    js

--- script.js
+++ script.js
@@ -7,7 +7,20 @@ let min=time.getMinutes();
 let sec=time.getSeconds();
 let day='';
 
-
-
+if(hr>12){
+    hr=hr-12;
+}
+if(hr==0){
+    h1=12;
+}
+if(sec<10){
+    sec='0'+sec;
+}
+if(min<10){
+    min='0'+min;
+}
+if(hr<10){
+    hr='0'+hr;
+}
 clock.innerHTML=hr+" : "+min+" : "+sec+' '+day
 })
